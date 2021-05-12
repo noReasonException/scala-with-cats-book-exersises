5166a5c12b9d8a52789832a8c92119a92b5a11b6
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-05-12 17:47:32 +0100

    C_1_3

---
+++ src/main/scala/C_1_3/Main.scala
@@ -0,0 +1,10 @@
+package C_1_3
+
+object Main {
+  val EXERSISE_1=FakeMainA
+  val EXERSISE_2=FakeMainB
+  val selector=EXERSISE_2
+  def main(args: Array[String]): Unit = {
+    selector.main(args)
+  }
+}
