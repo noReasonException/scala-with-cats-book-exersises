c767a658f1a46aee9c05561ff58cfd904f1e6a8a
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-05-13 07:39:05 +0100

    2.5

---
+++ src/main/scala/A_Playground/PlayMain.scala
@@ -0,0 +1,14 @@
+package A_Playground
+import cats.Monoid
+import cats.Semigroup
+
+import cats.instances.int._
+import cats.instances.set._
+
+import cats.syntax.monoid._
+import cats.syntax.semigroup._
+object PlayMain {
+  def main(args: Array[String]): Unit = {
+    //val a = Set.empty[Int] |+| Set.from(1::2::Nil)
+  }
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,10 @@
+import A_Playground.PlayMain
 import C_1_3.{MainC13A, MainC13B}
 import C_1_4.{MainC14A, MainC14B}
 import C_1_5.MainC15A
 import C_2_3.MainC23A
 import C_2_4.MainC24A
+import C_2_5.MainC25A
 import cats.Show
 import cats.instances.int._
 import cats.syntax.show._
@@ -10,6 +12,7 @@ import cats.Eq
 import cats.instances.option._
 import cats.syntax.eq._
 object Main {
+  val PLAY=PlayMain
   //chapter 1.3
   val EXERSISE_C_1_3_1=MainC13A
   val EXERSISE_C_1_3_2=MainC13B
@@ -19,11 +22,11 @@ object Main {
   //chapter 1.5
   val EXERSISE_C_1_5_1=MainC15A
 
-  //chapter 2.3
+  //chapter 2
   val EXERSISE_C_2_3_1=MainC23A
   val EXERSISE_C_2_3_2=MainC24A
-
-  val selector=EXERSISE_C_2_3_2
+  val EXERSISE_C_2_5_1=MainC25A
+  val selector=EXERSISE_C_2_5_1
   def main(args: Array[String]): Unit = {
     selector.main(args)
 
