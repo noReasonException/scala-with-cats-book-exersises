583c1a4ff37b6bccf622a3684ab90468102933a8
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-05-13 05:53:01 +0100

    1.5.1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,25 @@
 import C_1_3.{FakeMainC13A, FakeMainC13B}
 import C_1_4.{FakeMainC14A, FakeMainC14B}
+import C_1_5.FakeMainC15A
 import cats.Show
 import cats.instances.int._
 import cats.syntax.show._
+import cats.Eq
+import cats.instances.option._
+import cats.syntax.eq._
 object Main {
+  //chapter 1.3
   val EXERSISE_C_1_3_1=FakeMainC13A
   val EXERSISE_C_1_3_2=FakeMainC13B
-
+  //chapter 1.4
   val EXERSISE_C_1_4_1=FakeMainC14A
   val EXERSISE_C_1_4_2=FakeMainC14B
 
+  val EXERSISE_C_1_5_1=FakeMainC15A
   val selector=EXERSISE_C_1_4_2
   def main(args: Array[String]): Unit = {
     selector.main(args)
+
   }
 
 }
