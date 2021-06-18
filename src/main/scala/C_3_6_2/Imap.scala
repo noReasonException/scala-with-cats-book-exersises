99e0be9064ba283391ce1f43a203021a67d0f113
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-06-18 10:29:59 +0100

    imap box

--- src/main/scala/C_3_6_2/Imap.scala
+++ src/main/scala/C_3_6_2/Imap.scala
@@ -4,6 +4,8 @@ package C_3_6_2
 //instances
 //syntax
 
+final case class Box[A](element:A)
+
 trait Codec[A] { self=>
   def encode(element:A):String
   def decode(element:String):A
@@ -19,20 +21,33 @@ trait Codec[A] { self=>
 }
 
 object Instances{
-  implicit val StrCodec:Codec[String]=new Codec[String] {
+  implicit val strCodec:Codec[String]=new Codec[String] {
     override def encode(element: String): String = element
-
     override def decode(element: String): String = element
+  }
+  implicit def boxCodec[A](implicit boxedCodec:Codec[A]):Codec[Box[A]] = new Codec[Box[A]] {
+    override def encode(element: Box[A]): String = boxedCodec.encode(element.element)
 
+    override def decode(element: String): Box[A] = Box(boxedCodec.decode(element))
   }
 }
 
-object Syntax{
 
-}
+/**
+ * Note that the decode method of our Codec type class doesn’t account for failures.
+ * If we want to model more sophisticated relationships we can move beyond functors
+ * to look at lenses and optics.
+ */
 object Main{
+  import Instances._
+
+  val strToDouble:String=>Double=s=>s.toDouble
+  val doubleToStr:Double=>String=s=>s.toString
+  implicit val doubleCodec = strCodec.imap(strToDouble,doubleToStr)
+
   def main(args: Array[String]): Unit = {
-    print("hello world")
+    val box = Box(12.2)
+    print(boxCodec[Double].encode(box))
   }
 }
 
