dc0896bea36b67dc4743ff23834a58f6da49b644
Stefanos Stefanou <stefanos.stefanou@gbgplc.com>
2022-03-30 15:49:51 +0100

    why State[S,A] is Stack Safe?

--- src/main/scala/exersises/ch4_monads/H_PostOrderWithStateMonad.scala
+++ src/main/scala/exersises/ch4_monads/H_PostOrderWithStateMonad.scala
@@ -1,10 +1,12 @@
 package co.uk.noreasonexception
 package exersises.ch4_monads
 
+import cats.Monad
 import cats.data.State
 import cats.implicits._
 object H_PostOrderWithStateMonad{
   //  TODO Implement stack safety and invalid expression detection
+  // Wow , State seems to be stack safe, maybe because is expressed as StateT[Eval,A,B]
   type PostOrderCalc = State[List[String],String]
 
   private def evalOne(str:String):PostOrderCalc=State[List[String],String](state=>{
@@ -15,7 +17,7 @@ object H_PostOrderWithStateMonad{
     }
   })
   //Not Stack Safe
-  private def chain(n:Int,calc:String=>PostOrderCalc,expr:String):PostOrderCalc={
+  private def chain[F[_]:Monad,A](n:Int,calc:A=>F[A],expr:A):F[A]={
     if(n>0)calc(expr).flatMap(newExpr=>chain(n-1,calc,newExpr))
     else calc(expr)
   }
@@ -28,10 +30,17 @@ object H_PostOrderWithStateMonad{
       .run(Nil)
       .value._1.head.toInt
   }
+  //using iterateWhileM
+  def evaluate2(expression:String):Unit={
+    val result = expression.iterateWhileM(evalOne)(_.nonEmpty)
+    print(result.run(Nil).value)
+  }
 
   def main(args:Array[String]):Unit={
     val expression = "1 2 + 3 + 6 +"
     println(evaluatePostOrder(expression))
+
+    evaluatePostOrder((1 to 65000).mkString(" "))
   }
 
 
--- src/test/scala/exersise/ch4_monads/H_PostOrderWithStateMonad.scala
+++ src/test/scala/exersise/ch4_monads/H_PostOrderWithStateMonad.scala
@@ -5,7 +5,7 @@ import exersises.ch4_monads.Db
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-
+import cats.implicits._
 class H_PostOrderWithStateMonad extends AnyFlatSpec with should.Matchers {
 
   import exersises.ch4_monads.H_PostOrderWithStateMonad.evaluatePostOrder
@@ -18,9 +18,5 @@ class H_PostOrderWithStateMonad extends AnyFlatSpec with should.Matchers {
     val expression = "1 2 + 3 + 6 "
     evaluatePostOrder(expression) shouldBe 6
   }
-//  TODO Implement stack safety and invalid expression detection
-//  "EvaluatePostOrder" should "evaluate a invalid expression correctly" in {
-//    val expression = "+ +"
-//    evaluatePostOrder(expression) shouldBe 0
-//  }
+
 }
